9fe23d423ef5833a9b1dece4e18c9e5a586bfe1e
Ryjen1 <ryjenspark@gmail.com>
2025-12-26 15:20:03 +0100

    feat: install react-i18next and set up i18n configuration

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import "@reown/appkit/styles.css";
 import "./styles/appkit.css";
 import "./styles/button-animations.css";
+import "./lib/i18n";
 import App from "./App.tsx";
 import { Providers } from "./components/Providers.tsx";
 
